ebda378563f266be1136bb35fb35125c2b7ac4c3
ryuto3384 <ryuto.1128.n@icloud.com>
2024-07-29 15:15:14 +0900

    Initial Commit

---
+++ TrumpGames/TrumpGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrumpGamesApp.swift
+//  TrumpGames
+//
+//  Created by 中島瑠斗 on 2024/07/29.
+//
+
+import SwiftUI
+
+@main
+struct TrumpGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
